090af904f5165b063fc2b57fa3e6302f30b0799e
karmawangchuk07 <146674628+karmawangchuk07@users.noreply.github.com>
2026-03-20 18:30:57 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medbot',
+    version='0.1.0',
+    author='karma',
+    author_email='karmawangchukt07@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
